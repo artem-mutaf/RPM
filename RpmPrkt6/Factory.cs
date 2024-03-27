8e2ef589c64ac61e5ca7b29dfe397a21ade989e9
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-03-27 10:49:49 +0200

    Add files via upload

---
+++ RpmPrkt6/Factory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpmPrkt6
+{
+    class Factory:Shop
+    {
+        int quantity;
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+        public Factory() { }
+        public Factory(string nameshop,int quantity) :base(nameshop)
+        {
+            this.quantity = quantity;
+        }
+        public override string ToString()
+        {
+            return base.ToString()+Quantity;
+        }
+
+        new public static Factory Name()
+        {
+            Console.WriteLine("Введите название цеха: ");
+            string nameshop = Console.ReadLine();
+            Console.WriteLine("Введите кол-во производимого товара: ");
+            int quantity = int.Parse(Console.ReadLine());
+            return new Factory(nameshop,quantity);
+
+        }
+    }
+}
