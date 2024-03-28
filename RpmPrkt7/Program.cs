b52483a4e8721dd54766ec8d1bd6c6595a2e293b
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-03-28 20:53:55 +0200

    Add files via upload

---
+++ RpmPrkt7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpmPrkt7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stock1 = new Stock("Stock №1", 15);
+            Stock stock2 = new Stock("Stock №2", 10);
+
+            stock1.Add("Одежда");
+            stock1.Add("Кепки");
+            stock2.Add("Жидкости Chaser Lux 65mg");
+            stock2.Add("Наборы для самозамеса Chaser");
+            stock1.Delete("Одежда");
+
+            List<Stock> stocks = new List<Stock>();
+            stocks.Add(stock1);
+            stocks.Add(stock2);
+            
+
+            Console.WriteLine("Список складов: ");
+            foreach(var stock in stocks)
+            {
+                Console.WriteLine($"Название: {stock.name}, Вместимость: {stock.capacity}, Кол-во товаров: {stock.Inventory.Count}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RpmPrkt7/Stock.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpmPrkt7
+{
+    struct Stock
+    {
+        public string name { get; set; }
+        public int capacity { get; set; }
+        public List<string>Inventory { get; set; }
+
+        public Stock(string name,int capacity)
+        {
+            this.name = name;
+            this.capacity = capacity;
+            this.Inventory = new List<string>();
+        }
+        public void Add(string product)
+        {
+            if(Inventory.Count<capacity)
+            {
+                Inventory.Add(product);
+                Console.WriteLine($"Товар {product} добавлен на склад {name}");
+            }
+            else
+            {
+                Console.WriteLine($"Склад {name} полон...");
+            }
+        }
+
+        public void Delete(string product)
+        {
+            if(Inventory.Contains(product))
+            {
+                Inventory.Remove(product);
+                Console.WriteLine($"Товар {product} удален со склада {name}");
+            }
+            else
+            {
+                Console.WriteLine($"Товар {product} не найден на складе {name}");
+            }
+        }
+
+
+
+    }
+}
