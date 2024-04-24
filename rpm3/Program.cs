87c45dc91978d0c1ed3121275204fde1d7ee47a1
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-04-24 17:07:10 +0300

    Add files via upload

--- rpm3/Program.cs
+++ rpm3/Program.cs
@@ -10,39 +10,40 @@ namespace rpm3
     {
         static void Main(string[] args)
         {
-            
+            //Рекурсивный подход
+            Console.WriteLine("\tРекурсивный подход\n");
             int FibonacciRecursive(int n)
             {
-
-                
                 if (n == 0 || n == 1)
                     return n;
               
                     return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
-
-               
             }
-            int fib4 = FibonacciRecursive(4);
-            int fib5 = FibonacciRecursive(5);
-            int fib7 = FibonacciRecursive(7);
-            int fib9 = FibonacciRecursive(9);
+            int fib4 = FibonacciRecursive(0);
+            int fib5 = FibonacciRecursive(1);
+            int fib7 = FibonacciRecursive(2);
+            int fib9 = FibonacciRecursive(10);
             Console.WriteLine($"4 число фибоначчи = {fib4}");
             Console.WriteLine($"5 число фибоначчи = {fib5}");
             Console.WriteLine($"7 число фибоначчи = {fib7}");
-            Console.WriteLine($"9 число фибоначии = {fib9}");
+            Console.WriteLine($"9 число фибоначии = {fib9}\n");
 
+            Console.WriteLine("\tИтеративный подход\n");
+            //Итеративный подход
             int FibonacciIterative(int n)
             {
                 if (n <= 1)
                     return n;
                 int prev = 0;
                 int current = 1;
+                Console.WriteLine("Числа Фибоначчи от 1 до 16");
                 for (int i = 2; i <= n; i++)
                 {
                     int next = prev + current;
                     prev = current;
                     current = next;
-                    Console.WriteLine($"\t{next}");
+                    
+                    Console.WriteLine($"Число {i} = {next}");
                 }
                 return current;
                 
